6eff7f0e2137543a21e6324727a0e98f896a0b7b
Yena-Park <yena8317@gmail.com>
2018-09-20 03:01:35 -0400

    initial commit :)

---
+++ src/assign1/DBConnector.java
@@ -0,0 +1,7 @@
+//StudentID : 991496627, Student Name : Yena Park
+
+package assign1;
+
+public class DBConnector {
+
+}
---
+++ src/assign1/PublicLibrary.java
@@ -0,0 +1,12 @@
+//StudentID : 991496627, Student Name : Yena Park
+
+package assign1;
+
+public class PublicLibrary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
